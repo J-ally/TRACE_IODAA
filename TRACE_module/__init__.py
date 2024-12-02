85ed6c0c632927aa6a445c2ec1cf77f8b3db2c07
AdrienBouchet1 <adrien.bouchet@agroparistech.fr>
2024-12-02 10:05:19 +0100

    début du module

---
+++ TRACE_module/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 10:03:33 2024
+
+@author: bouchet
+"""
+
---
+++ TRACE_module/preprocessing.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 10:01:10 2024
+
+@author: bouchet
+"""
+
