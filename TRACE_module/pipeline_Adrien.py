583ddf7fb8489e2f29969eadbb6a39d5ddb44206
er <elise.ext@gmail.com>
2025-01-22 19:55:54 +0100

    Added boxplot to the pipeline

--- TRACE_module/pipeline_Adrien.py
+++ TRACE_module/pipeline_Adrien.py
@@ -13,11 +13,11 @@ Created on Mon Jan 13 15:51:20 2025
 
 import preprocessing as pp
 
-import visualisation as vi 
+import visualisation as vi
 from descriptive_analysis import from_stack_to_number_of_interaction_sequences,from_distance_to_sequences_vector, from_distances_to_sequences_stack,from_seq_to_average_interaction_time,from_seq_to_daily_interactions
 
 import numpy as np
-import os 
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -31,14 +31,14 @@ if os.getcwd().endswith("TRACE_module"):
 elif os.getcwd().endswith("TRACE_IODAA"):
     new_path = os.getcwd() + os.sep + "UTILS_module"
 sys.path.insert(0, new_path) # insert path to be able to import file afterwards, allows for imports wherever the script is called from
-    
+
 from UTILS_files import test_import
 if test_import() : # Testing the import
     from UTILS_files import get_all_files_within_one_folder # Importing the wanted function
 else :
     print("Import failed")
-    
-    
+
+
 
 import preprocessing as pp
 
@@ -56,7 +56,7 @@ list_files=list()
 ##################################
 
 
-t1=time.perf_counter() 
+t1=time.perf_counter()
 
 list_files = get_all_files_within_one_folder(folder, True, extension=".parquet")
 
@@ -67,7 +67,7 @@ t2=time.perf_counter()
 
 
 folder_savings =  os.sep.join([output_dir,"savings"])
-if not  os.path.isdir(folder_savings) : 
+if not  os.path.isdir(folder_savings) :
     os.makedirs(folder_savings)
 
 data=pp.concatenate_df(list_files)
@@ -77,8 +77,8 @@ data=pp.concatenate_df(list_files)
 ##########################################
 t3=time.perf_counter()
 
-data=pp.transform_rssi_to_distance(data)
-t4=time.perf_counter() 
+#data=pp.transform_rssi_to_distance(data)
+t4=time.perf_counter()
 
 ###################################################################
 #ETAPE 5 - Création d'un stack de matrice d'adjacence symétriques #
@@ -90,27 +90,30 @@ list_id=list(pd.unique(data["accelero_id"]))
 stack,list_timesteps=pp.create_stack(data,list_id)
 t5=time.perf_counter()
 
-
-
 ###################################################################
 #ETAPE 6- Analyses                                                #
 ###################################################################
 
 
-start_time = pd.Timestamp('2024-03-20T08:39:00.000000000')
-end_time = pd.Timestamp('2024-04-10T16:36:00.000000000')
+#start_time = pd.Timestamp('2024-03-20T08:39:00.000000000')
+#end_time = pd.Timestamp('2024-04-10T16:36:00.000000000')
+
+start_time = pd.Timestamp('2024-03-22T08:39:00.000000000')
+end_time = pd.Timestamp('2024-04-8T16:36:00.000000000')
 
 
-stack,list_timesteps=pp.crop_start_end_stack(stack=stack, 
-                         list_timesteps = list_timesteps , 
+stack,list_timesteps=pp.crop_start_end_stack(stack=stack,
+                         list_timesteps = list_timesteps ,
                          start = start_time,
                          end = end_time)
 
-
+###
 
 distances_clean=stack
-t5b=time.perf_counter() 
+t5b=time.perf_counter()
 #####
+#print(distances_clean[0 ,:, : ])
+print(distances_clean.shape)
 
 distances_clean=np.where(distances_clean==0,np.nan,distances_clean)
 ## Creation a a sequence matrix
@@ -118,22 +121,19 @@ matrice_seq = from_distances_to_sequences_stack(distances_clean)
 
 ###
 
-
-
-
 number_of_interaction_sequences = from_stack_to_number_of_interaction_sequences(matrice_seq)
 
 
 number_of_daily_interaction = from_seq_to_daily_interactions(matrice_seq,list_timesteps)
 average_duration_of_an_interaction = from_seq_to_average_interaction_time(matrice_seq)
 
-t6=time.perf_counter() 
+t6=time.perf_counter()
 
 #####################
 #Visualisation#
 #####################
 
-   
+
 vi.heatmap_interactions_number(number_of_interaction_sequences,list_id)
 
 
@@ -144,4 +144,5 @@ vi.barplot_interaction_cows(number_of_daily_interaction,average_duration_of_an_i
 
 print("--BENCHMArk-- \n ==========\n étape 1 - localisation des données: {} \n étape 2 - concaténation des fichiers et création d'un dataframe unique ': {} \n étape 3 -transformation des signaux RSSI en distance {} \n étape 4 - Mise en forme des données sous forme matricielle: {} \n étape 5 : Analyse descriptive :{} ".format(t2-t1,t3-t2,t4-t3,t5-t4,t6-t5))
 
-
+vi.boxplot_average_time_number_interactions(number_of_daily_interaction,
+                                             average_duration_of_an_interaction)
